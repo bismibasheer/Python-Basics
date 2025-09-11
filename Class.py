d37637093edc30b009164b19538044ddba14c7df
bismibasheer <bismibasheer2001@gmail.com>
2025-09-11 16:31:23 +0530

    New commits

--- Class.py
+++ Class.py
@@ -1,4 +1,4 @@
-class student:
+"""class student:
     institude="oneteam"
 
     def get_details(self,n,a):
@@ -14,4 +14,21 @@ st1.get_details("Bismi",23)
 st2.get_details("Ano",24)
 print(st1.name)
 print(st2.name)
-st2.display()
+st2.display()"""
+
+
+class student:
+    institude="oneteam"
+
+    def __init__(self,n,a):
+        self.name=n
+        self.age=a
+
+    def display(self):
+        print(f"your name is {self.name} and you are {self.age} years old")  
+st1=student("Bismi",23)
+st2=student("Ano",24)
+st1.display()
+
+
+
